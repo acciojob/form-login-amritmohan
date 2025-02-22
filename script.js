134267701ad9bbc8567e2b2b116ecf719ded0eec
@amritmohan <amritmohan2284@gmail.com>
2025-02-23 00:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	let form = getElementById(form1);
+	let firstName = forms.elements["fname"].value.trim();
+	let lastName = forms.elements["lname"].value.trim();
 
+	if(firstName === "" || lastName === ""){
+		alert("Please enter both firstname and lastname.")
+		return;
+	}
+	alert(`${firstName} ${lastName}`);
 }
